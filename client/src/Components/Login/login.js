ba634926b7574d9d912a8284e55a57c1be6ce478
Javier Verde Marín <73306559+javVM@users.noreply.github.com>
2021-11-15 17:16:08 +0100

    Update login.js

--- client/src/Components/Login/login.js
+++ client/src/Components/Login/login.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import linkedin_logo from '../../Images/linkedin_logo.png';
 import styles from './login.module.css'
-
+import {context} from '../../contextProvider.js'
 const Login = () => { 
-    
-    const verifyLinkedIn = () => { 
-        window.location.href ="http://localhost:3001/auth/linkedin"}
- 
-    return <div className={styles.loginGeneral}>
-        <div className={styles.loginBox}>
-            <h1>Login</h1>
-            <div className={styles.loginButton} onClick={verifyLinkedIn}>
-                <img src={linkedin_logo} alt="Linkedin Logo"/>
-                <p>Verify your identity with LinkedIn</p>
-            </div>
-            <div className={styles.reminderText}>
-                <p>Your identity or personal data will not be published or stored anywhere</p>
+
+    const user = React.useContext(context)
+
+    if(!user){
+        const verifyLinkedIn = () => { 
+            window.location.href ="http://localhost:3001/auth/linkedin"}
+        
+        return <div className={styles.loginGeneral}>
+            <div className={styles.loginBox}>
+                <h1>Login</h1>
+                <div className={styles.loginButton} onClick={verifyLinkedIn}>
+                    <img src={linkedin_logo} alt="Linkedin Logo"/>
+                    <p>Verify your identity with LinkedIn</p>
+                </div>
+                <div className={styles.reminderText}>
+                    <p>Your identity or personal data will not be published or stored anywhere</p>
+                </div>
             </div>
         </div>
-    </div>
+    }
+    else{
+
+        return <div><h1>Welcome</h1></div>
+    }
+    
 }
 
-export default Login;
+export default Login
