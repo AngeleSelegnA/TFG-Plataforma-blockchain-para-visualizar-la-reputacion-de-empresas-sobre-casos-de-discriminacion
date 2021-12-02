725143bcdfbec49dd392e84ea7f2423afc554628
Javier Verde Marín <73306559+javVM@users.noreply.github.com>
2021-12-02 23:37:48 +0100

    index.js Subido

--- server/index.js
+++ server/index.js
@@ -1,20 +1,26 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 const passport = require('passport');
 const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 const genName = require("randomstring");
-var session = require('express-session')
-var User = require('./User.module.js')
+var session = require('express-session');
+var User = require('./User.module.js');
+
+//Para conectarse al smart contract
+const Web3 = require('web3');
+const infuraUrl = process.env.INFURA_URL;
+//Crea una instancia para comunicarse con el nodo indicado
+const init = async() => new Web3(infuraUrl);
 
 //Servidor
-const app = express()
+const app = express();
 const port = 3001 //Se esta ejecutando en local y en 3000 se ejecuta la web
 
 app.listen(port, () => {   
-  console.log("The server is running")
-})
+  console.log("The server is running");
+});
 
 //Conexion con BBDD y formato de los datos
 try {
@@ -22,28 +28,28 @@ mongoose.connect(`${process.env.MONGO_START}${process.env.DB_USER}:${process.env
   useNewUrlParser: true, 
   useUnifiedTopology: true
 }, ()=>{
-  console.log("Successfully connected to DB")
+  console.log("Successfully connected to DB");
 })
 }catch(e){
-  console.error("Could not connect")
+  console.error("Could not connect");
 }
 
 //Habilitar cross-site requests PERO solo desde nuestra pagina de react
-app.use(cors({origin : `${process.env.REACT_PAGE}`, credentials: true}))
+app.use(cors({origin : `${process.env.REACT_PAGE}`, credentials: true}));
 
 //Recibir las respuestas como un json y poder dividirlos en clave-valor
-app.use(express.json())
-app.use(express.urlencoded({extended : true}))
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 
 //Sesion y Oauthv2
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true
-}))
+}));
 
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Definimos la estrategia
 passport.use(new LinkedInStrategy({
@@ -57,50 +63,58 @@ passport.use(new LinkedInStrategy({
   process.nextTick(function () {
     //vamos a comprobar si el usuario está en la bbdd, si no, se añade
     User.findOne({'l_id' : profile.id}, (err, doc) =>{
-      if(err) return done(err, null)
+      if(err) return done(err, null);
       else if(!doc){
           //Guardamos el id y el usuario generado de forma aleatoria
-          const username = genName.generate({length:15,charset: 'alphanumeric'})
-          const new_user = new User({ l_id : profile.id , u_name : username})
-          new_user.save(function(){})
+          const username = genName.generate({length:15,charset: 'alphanumeric'});
+          const new_user = new User({ l_id : profile.id , u_name : username});
+          new_user.save(function(){});
 
           //Se realiza callback con el nuevo usuario creado
-          return done(null,new_user)
+          return done(null,new_user);
       }
       else{
           //Si el usuario ya existe se devuelve el documento
-          return done(null, doc)
+          return done(null, doc);
       }
     })
     
-  })
-}))
+  });
+}));
 
 app.get('/auth/linkedin',
   passport.authenticate('linkedin'),
   function(req, res){
     // Esta funcion no se llama, linkedin se encarga de la autenticacion
-})
+});
 
 app.get('/auth/linkedin/callback', (req, res, next) => {
   passport.authenticate('linkedin', (err, user, info) => {
    if (err || !user) {
      // Si el usuario cancela el permiso
-     return res.redirect(`${process.env.REACT_PAGE}/login`)
+     return res.redirect(`${process.env.REACT_PAGE}/user`);
    }
-   req.login(user, (err) => { if (err) { return next(err) }
+   req.login(user, (err) => { if (err) { return next(err); }
    ///Si el usuario da permiso 
-  res .redirect(`${process.env.REACT_PAGE}`)
+  res .redirect(`${process.env.REACT_PAGE}`);
    })
   })(req, res, next)
- })
+ });
 
 //Se va a guardar el id del usuario para la cookie
-passport.serializeUser((user, done) => { return done(null, user.id)})
+passport.serializeUser((user, done) => { return done(null, user.id); });
 //Se saca de la base de datos el usuario y se puede acceder a ella mediante req.user
-passport.deserializeUser((id, done) => { User.findById(id, (err, user) => {return done (null, user) 
-})})
+passport.deserializeUser((id, done) => { User.findById(id, (err, user) => {return done (null, user);
+})});
 
 
 //Envia al frontend el valor del usuario
-app.get("/getuser" , (req, res) => { res.send(req.user) })
+app.get('/getuser' , (req, res) => { res.send(req.user); });
+
+//Se cierra la sesion
+app.get('/logout', function(req, res){
+  req.logout();
+  req.session.destroy(function (err) {
+    res.redirect('http://localhost:3000/');
+  });
+});
