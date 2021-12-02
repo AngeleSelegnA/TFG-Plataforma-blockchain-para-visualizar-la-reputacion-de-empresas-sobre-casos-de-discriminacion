a335e561642891696d83d56a477aacd283f2351b
Javier Verde Marín <73306559+javVM@users.noreply.github.com>
2021-12-02 23:30:34 +0100

    App.js Actualizado

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
+import React from 'react'
 import { BrowserRouter as Router, Switch , Route } from 'react-router-dom';
 import Home from './Components/Home/home.js';
-import Login from './Components/Login/login.js';
-import Menu from './Components/Menu/menu.js'
+import User from './Components/User/user.js';
+import Menu from './Components/Menu/menu.js';
 
 function App() {
+
   return (
     <div className="App">
       <Router>
         <Menu />
         <Switch>
-          <Route path="/login" component={ Login }></Route>
+          <Route path="/user" component={ User }></Route>
           <Route path="/" component={ Home }></Route>
         </Switch>
       </Router>
