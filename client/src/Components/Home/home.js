33e406f5b82d7870e965eec2abb30ea9c1577adb
Javier Verde Marín <73306559+javVM@users.noreply.github.com>
2021-12-13 20:43:11 +0100

    home.js Actualizado

--- client/src/Components/Home/home.js
+++ client/src/Components/Home/home.js
@@ -1,33 +1,34 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState} from 'react';
 import Web3 from 'web3';
 import * as constants from './../../constantFile.js';
+import complaintContract from './../../complaintContract.json';
 
 const Home = () => {
 
     const [companies, setCompanies] = useState([]);
     const [reputation, setReputation] = useState([]);
-    const [card, setCard] = useState([]);
    
-    //Para conectarse al smart contract
     useEffect(() => {
+    //Para conectarse al smart contract
         const infuraUrl = constants.INFURA_URL;
 
         //Crea una instancia para comunicarse con el nodo indicado
-        const web3 = async() => new Web3(infuraUrl);
+        const web3 = new Web3(new Web3.providers.HttpProvider(infuraUrl));
         //Se conectaria con el contrato
-        //const contract = web3.eth.Contract(complaintContract.abi, constants.CONTRACT_ADDRESS);
+        const contract = new web3.eth.Contract(complaintContract.abi, constants.CONTRACT_ADDRESS);
 
-        /*
-        contract.methods.getCompanies().call().then(setCompanies);
-        companies.forEach(element=> {
-            contract.methods.getCompanies(element).call().then(response => setReputation([...reputation, response]));
-        });
-        */
-        setCard([companies.map((e, i) => [e, reputation[i]])]);   
+        //Obtenemos el array de empresas
+        contract.methods.getCompanies().call().then(response => setCompanies(response));
+        
+        //Obtenemos sus reputaciones
+        contract.methods.getReputation().call().then(response => setReputation(response));
     
-    }, []);
+    },[]);
     
-    let companyCards = card.map(item => <div><h3>{item[0]} {item[1]}</h3></div>)
+    //Unimos reputaciones y empresas
+    let card = companies.map((e, i) => [e, reputation[i]]);
+    //Creamos componentes (para las cartas)
+    let companyCards = card.map(item => <div><h3>{item[0]} {item[1]}</h3></div>);
 
     return <div>
         <h1>This is the homescreen</h1>
