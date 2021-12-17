0c38a575be2730996f6642ee2db95d3809d9f6dc
Javier Verde Marín <73306559+javVM@users.noreply.github.com>
2021-12-17 20:17:57 +0100

    constantFile Actualizado

--- client/src/constantFile.js
+++ client/src/constantFile.js
@@ -1,3 +1,5 @@
 export const INFURA_URL='infura url here';
 export const SERVER_URL='server url here';
 export const CONTRACT_ADDRESS='0xaF3105f24B6dca8dC31d8562806C5a98997D038f';
+export const PRIVATE_KEY= 'private key here';
+export const ADDRESS = '0x74edbc557d7c13a084623a2c0aa7f1fcaeb89b9d';
